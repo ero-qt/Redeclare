73d38a0f16637b25e475757e8f0a9eda2baf90fc
ero-qt <ero-qt@tuta.io>
2026-09-10 14:50:58 +0200

    Add symbol checks and attribute lookup

---
+++ src/Redeclare/Roslyn/AttributeExtensions.cs
@@ -0,0 +1,58 @@
+using Microsoft.CodeAnalysis;
+using System.Collections.Generic;
+
+namespace Redeclare;
+
+/// <summary>
+///     Provides methods that find attributes on symbols by their class. Resolve the class once with
+///     <c>Compilation.GetTypeByMetadataName</c> and compare symbols.
+/// </summary>
+internal static class AttributeExtensions
+{
+    /// <summary>
+    ///     Gets a value indicating whether the attribute is of <paramref name="attributeClass"/>. A constructed
+    ///     generic attribute matches its definition.
+    /// </summary>
+    public static bool IsOfClass(this AttributeData attribute, INamedTypeSymbol attributeClass)
+    {
+        return attribute.AttributeClass is { } actual && actual.Is(attributeClass);
+    }
+
+    /// <summary>
+    ///     Gets a value indicating whether the symbol carries an attribute of this class.
+    /// </summary>
+    public static bool HasAttribute(this ISymbol symbol, INamedTypeSymbol attributeClass)
+    {
+        return symbol.GetAttribute(attributeClass) is not null;
+    }
+
+    /// <summary>
+    ///     The first attribute of this class on the symbol, or <see langword="null"/>.
+    /// </summary>
+    public static AttributeData? GetAttribute(this ISymbol symbol, INamedTypeSymbol attributeClass)
+    {
+        foreach (var attribute in symbol.GetAttributes())
+        {
+            if (attribute.IsOfClass(attributeClass))
+            {
+                return attribute;
+            }
+        }
+
+        return null;
+    }
+
+    /// <summary>
+    ///     Every attribute of this class on the symbol.
+    /// </summary>
+    public static IEnumerable<AttributeData> GetAttributes(this ISymbol symbol, INamedTypeSymbol attributeClass)
+    {
+        foreach (var attribute in symbol.GetAttributes())
+        {
+            if (attribute.IsOfClass(attributeClass))
+            {
+                yield return attribute;
+            }
+        }
+    }
+}
